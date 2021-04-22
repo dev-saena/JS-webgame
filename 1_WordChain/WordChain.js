92e9869bb2af1bd232ce83ca3d1bb1f643a36cff
saena <dev.saena@gmail.com>
2021-04-22 23:10:50 +0900

    Comment WordChain.js

--- 1_WordChain/WordChain.js
+++ 1_WordChain/WordChain.js
@@ -14,11 +14,13 @@ var 결과창 = document.createElement('div');
 
 폼.addEventListener('submit', function 콜백함수 (e) {
   e.preventDefault();
+  // 제시어 끝 글자와 입력어 첫 글자가 같을 경우
   if (단어.textContent[단어.textContent.length - 1] === 입력창.value[0]) {
     결과창.textContent = '딩동댕';
     단어.textContent = 입력창.value;
     입력창.value = '';
     입력창.focus();
+  // 제시어 끝 글자와 입력어 첫 글자가 다를 경우
   } else {
     결과창.textContent = '땡';
     입력창.value = '';
